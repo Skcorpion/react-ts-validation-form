1ea57b665c4eed98a7be83d894ceed7e4c0aab1d
Roman Zubko <romanzubko93@gmail.com>
2023-05-26 16:43:00 +0300

    task #10 complete validation w/ Formik

--- src/ValidationForm.tsx
+++ src/ValidationForm.tsx
@@ -1,89 +1,124 @@
-import { ChangeEvent, FormEvent, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Formik, FormikErrors } from "formik";
+import * as yup from "yup";
+import { InputGroup } from "react-bootstrap";
+import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+const schema = yup.object().shape({
+  username: yup.string().required(),
+  email: yup.string(),
+  password: yup.string(),
+});
 
 function ValidationFrom() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [validated, setValidated] = useState(false);
-
-  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const username = event.target.value;
-    setValidated(true);
-    if (!Array.isArray(username.match(/[^a-zA-Z]/g))) {
-      setUsername(username);
-      setValidated(false)
-    }
-  };
-  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const email = event.target.value;
-    setEmail(email);
-  };
-  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const password = event.target.value;
-    setPassword(password);
-  };
-  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    console.log("click");
-    setValidated(true);
+  const [showPass, setShowPass] = useState(false);
+  const handlePassVisibility = () => {
+    setShowPass((prevState) => !prevState);
   };
   return (
-    <Form
-      className="mx-auto mt-3 p-3 border border-3 rounded"
-      noValidate
-      validated={validated}
-      onSubmit={handleFormSubmit}
+    <Formik
+      validationSchema={schema}
+      initialValues={{ username: "", email: "", password: "" }}
+      validate={(values) => {
+        const errors: FormikErrors<typeof values> = {};
+        if (!values.username) {
+          errors.username = "Required";
+        } else if (/[^a-zA-Z]/g.test(values.username)) {
+          errors.username = "Enter letters without spaces";
+        }
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (!/^[\w.-]+@gmail\.com$/.test(values.email)) {
+          errors.email = "Enter only email addresses on gmail";
+        }
+        if (!values.password) {
+          errors.password = "Required";
+        } else if (
+          !/(?=(?:.*[A-Z]){5})(?=(?:.*[!@#$%^&*()_+={}[\]\\|:;'<>,.?/]){6})(?=(?:.*\-){2})^[A-Za-z0-9!@#$%^&*()_\-+={}[\]\\|:;'<>,.?/]+$/.test(
+            values.password
+          )
+        ) {
+          errors.password =
+            "Your password must contain five (5) capital letters, six (6) symbols and two hyphens (-) in any order.";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
     >
-      <Form.Group className="mb-3" controlId="formBasicUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          required
-          type="text"
-          placeholder="Enter username"
-          value={username}
-          onChange={handleUsernameChange}
-        />
-        <Form.Control.Feedback type="invalid">
-          Enter letters without spaces
-        </Form.Control.Feedback>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          required
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <Form.Control.Feedback type="invalid">
-          Enter only email addresses on gmail
-        </Form.Control.Feedback>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPasswod">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          required
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <Form.Control.Feedback type="invalid">
-          Your password must contain five (5) capital letters, six (6) symbols
-          and two hyphens (-) in any order.
-        </Form.Control.Feedback>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+      {({
+        handleSubmit,
+        handleChange,
+        values,
+        touched,
+        isSubmitting,
+        errors,
+      }) => (
+        <Form
+          className="mx-auto mt-3 p-3 border border-3 rounded"
+          noValidate
+          onSubmit={handleSubmit}
+        >
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              value={values.username}
+              onChange={handleChange("username")}
+              isValid={touched.username && !errors.username}
+              isInvalid={!!errors.username}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.username}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={values.email}
+              isValid={touched.email && !errors.email}
+              isInvalid={!!errors.email}
+              onChange={handleChange("email")}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.email}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <InputGroup hasValidation>
+              <Form.Control
+                type={!showPass ? "password" : "text"}
+                placeholder="Password"
+                value={values.password}
+                isValid={touched.password && !errors.password}
+                isInvalid={!!errors.password}
+                onChange={handleChange("password")}
+              />
+              <InputGroup.Text className="pointer" onClick={handlePassVisibility}>
+                  {!showPass ? <AiFillEyeInvisible /> : <AiFillEye />}
+              </InputGroup.Text>
+              <Form.Control.Feedback type="invalid">
+              {errors.password}
+            </Form.Control.Feedback>
+            </InputGroup>
+            
+          </Form.Group>
+          <Button variant="primary" type="submit" disabled={isSubmitting}>
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
